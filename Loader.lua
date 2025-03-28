a87804da544b2a69ea661b2c64573a5b82f975df
HmSwitchLoaderz <sleepyyshadows@gmail.com>
2025-03-28 17:04:56 -0400

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,10 @@
+--[[
+
+        Triplex™
+        @ev6k
+        Version 1.0.0
+
+]]
+
+getgenv().Version = "DaHood" --// DaHood, ThaBronx
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HmSwitchLoaderz/Triplex/refs/heads/main/OBF_Src.lua"))()
